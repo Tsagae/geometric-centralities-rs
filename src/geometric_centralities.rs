ee37deb20abf59e8a1fc766c0d4eeacc7866c039
Matteo Zagheno <matteo2309@hotmail.com>
2024-11-07 15:15:44 +0100

    removed bfs pl

--- src/geometric_centralities.rs
+++ src/geometric_centralities.rs
@@ -1,7 +1,7 @@
 use atomic_counter::AtomicCounter;
 use common_traits::Number;
 use crossbeam_channel::unbounded;
-use dsi_progress_logger::{ProgressLog, ProgressLogger};
+use dsi_progress_logger::{progress_logger, ProgressLog, ProgressLogger};
 use rayon::iter::IndexedParallelIterator;
 use rayon::iter::IntoParallelRefMutIterator;
 use rayon::iter::ParallelIterator;
@@ -150,185 +150,6 @@ impl<G: RandomAccessGraph + Sync> GeometricCentralities<'_, G> {
         }
     }
 
-    /*
-    pub fn compute_with_2_channels(&mut self) {
-        let num_of_nodes = self.graph.num_nodes();
-        self.closeness = vec![-1f64; num_of_nodes];
-        self.harmonic = vec![-1f64; num_of_nodes];
-        self.lin = vec![-1f64; num_of_nodes];
-        self.exponential = vec![-1f64; num_of_nodes];
-        self.reachable = vec![0; num_of_nodes];
-
-        let logging = self.logging;
-        let num_threads = min(self.graph.num_nodes(), self.num_of_threads);
-        if logging {
-            self.pl.start(&format!(
-                "Computing geometric centralities with {num_threads} threads..."
-            ));
-        }
-        let shared_pl = Arc::new(Mutex::new(&mut self.pl));
-
-        let mut thread_pool_builder = rayon::ThreadPoolBuilder::new();
-        thread_pool_builder = thread_pool_builder.num_threads(num_threads);
-        let thread_pool = thread_pool_builder
-            .build()
-            .expect("Error in building thread pool");
-
-        let (send_to_thread, receive_in_thread) = unbounded();
-        let (send_out_of_thread, receive_from_thread) = unbounded();
-
-        let mut sent = 0;
-        let mut received = 0;
-        for i in 0..min(num_threads * 2, num_of_nodes) {
-            send_to_thread.send((false, i)).unwrap();
-            sent += 1;
-        }
-
-        thread_pool.in_place_scope(|scope| {
-            for i in 0..num_threads {
-                let local_send_out_of_thread = (&send_out_of_thread).clone();
-                let local_receive_in_thread = (&receive_in_thread).clone();
-                let num_of_nodes = self.graph.num_nodes();
-                let graph = self.graph;
-                let local_pl = Arc::clone(&shared_pl);
-                scope.spawn(move |_| {
-                    //println!("Started thread {}", i);
-                    let num_of_nodes = num_of_nodes;
-                    let mut distances = vec![-1; num_of_nodes];
-                    let mut queue = VecDeque::new();
-                    loop {
-                        let (stop, target_node) = local_receive_in_thread
-                            .recv()
-                            .expect("Failed receiving in thread");
-                        if stop {
-                            break;
-                        }
-
-                        let centralities =
-                            Self::single_visit(&graph, target_node, &mut distances, &mut queue);
-                        local_send_out_of_thread
-                            .send((target_node, centralities))
-                            .expect(&format!(
-                                "Failed send out of thread {i} target_node: {target_node}"
-                            ));
-                        if logging {
-                            let mut pl = local_pl.lock().expect("Error in taking mut pl");
-                            pl.update();
-                        }
-                    }
-                });
-            }
-            let first_sent = sent;
-            for _ in 0..(num_of_nodes - first_sent) {
-                let (
-                    node,
-                    GeometricCentralityResult {
-                        closeness,
-                        harmonic,
-                        lin,
-                        exponential,
-                        reachable,
-                    },
-                ) = receive_from_thread
-                    .recv()
-                    .expect("Failed receiving from thread");
-                self.closeness[node] = closeness;
-                self.harmonic[node] = harmonic;
-                self.lin[node] = lin;
-                self.exponential[node] = exponential;
-                self.reachable[node] = reachable;
-                received += 1;
-
-                send_to_thread
-                    .send((false, sent))
-                    .expect("Failed sending to thread");
-                sent += 1;
-            }
-
-            for _ in 0..first_sent {
-                let (
-                    node,
-                    GeometricCentralityResult {
-                        closeness,
-                        harmonic,
-                        lin,
-                        exponential,
-                        reachable,
-                    },
-                ) = receive_from_thread
-                    .recv()
-                    .expect("Failed receiving from thread");
-                self.closeness[node] = closeness;
-                self.harmonic[node] = harmonic;
-                self.lin[node] = lin;
-                self.exponential[node] = exponential;
-                self.reachable[node] = reachable;
-                received += 1;
-            }
-
-            for i in 0..num_threads {
-                send_to_thread
-                    .send((true, i))
-                    .expect("Failed sending to thread");
-            }
-        });
-        if logging {
-            let mut pl = shared_pl.lock().expect("Error in taking mut pl");
-            pl.done();
-            eprintln!("{}", pl.to_string());
-        }
-    }
-    */
-    /*
-    pub fn compute_with_par_iter(&mut self, chunk_size: usize) {
-        self.results = vec![
-            GeometricCentralityResult {
-                closeness: -1f64,
-                harmonic: -1f64,
-                lin: -1f64,
-                exponential: -1f64,
-                reachable: 0,
-            };
-            self.graph.num_nodes()
-        ];
-
-        let logging = self.logging;
-        if logging {
-            self.pl.start(&format!(
-                "Computing geometric centralities with max threads and {chunk_size} chunks..."
-            ));
-        }
-        let shared_pl = Mutex::new(&mut self.pl);
-
-        self.results
-            .par_iter_mut()
-            .enumerate()
-            .chunks(chunk_size)
-            .for_each(|mut vec_chunk| {
-                thread_local!(static DISTANCES: RefCell<Vec<i64>> = RefCell::new(Vec::new()));
-                thread_local!(static QUEUE: RefCell<VecDeque<usize>> = RefCell::new(VecDeque::new()));
-
-                vec_chunk.iter_mut().for_each(|(node, ref mut item)| {
-                    DISTANCES.with_borrow_mut(|distances| {
-                        QUEUE.with_borrow_mut(|queue| {
-                            **item = Self::single_visit(self.graph, *node, distances, queue);
-                        })
-                    });
-                });
-                if logging {
-                    let mut pl = shared_pl.lock().expect("Error in taking mut pl");
-                    pl.update_with_count(chunk_size);
-                }
-            });
-
-        if logging {
-            let mut pl = shared_pl.lock().expect("Error in taking mut pl");
-            pl.done();
-            eprintln!("{}", pl.to_string());
-        }
-    }
-    */
-
     fn single_visit(
         start: usize,
         bfs: &mut SingleThreadedBreadthFirstVisit<(), &G>,
@@ -342,9 +163,7 @@ impl<G: RandomAccessGraph + Sync> GeometricCentralities<'_, G> {
         let base = DEFAULT_ALPHA;
 
         bfs.reset();
-        bfs.visit(
-            start,
-            |args| {
+        bfs.visit(start, |args| {
                 let d = args.distance;
 
                 reachable += 1;
@@ -358,9 +177,7 @@ impl<G: RandomAccessGraph + Sync> GeometricCentralities<'_, G> {
                 harmonic += hd;
                 exponential += ed;
                 Ok(())
-            },
-            &mut ProgressLogger::default(),
-        )
+            }, &mut Option::<ProgressLogger>::None)
         .expect("Error in bfs");
         if closeness == 0f64 {
             lin = 1f64;

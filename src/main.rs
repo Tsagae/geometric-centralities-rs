288bcae93be6fcf23498eb88b52a2a90a1c0952b
Matteo Zagheno <matteo2309@hotmail.com>
2024-11-07 11:29:48 +0100

    added geometric centralities code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use crate::geometric_centralities::GeometricCentralities;
+use webgraph::prelude::BvGraph;
+
+mod geometric_centralities;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let path = "/home/matteo/Documents/tesi/example_graphs/uk-2007-05@100000/".to_owned();
+    let graph = BvGraph::with_basename(path + "uk-2007-05@100000")
+        .load()
+        .expect("Failed loading graph");
+    let mut geom = GeometricCentralities::new(&graph, 0, true);
+    geom.compute_with_par_iter(10);
+    println!("Done");
 }

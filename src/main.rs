0dbc186d095122def864b59a349579cfaa28fba6
Matteo Zagheno <matteo2309@hotmail.com>
2024-11-19 16:59:04 +0100

    removed default test and min_len_iter 10M benchmarks

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::geometric_centralities::GeometricCentralities;
-use dsi_progress_logger::{ProgressLogger};
+use dsi_progress_logger::ProgressLogger;
 use log::info;
 use std::env;
 use webgraph::prelude::BvGraph;
@@ -14,17 +14,12 @@ fn main() {
         .load()
         .expect("Failed loading graph");
     let mut geom = GeometricCentralities::new(&graph, 0);
-    info!("-------------- Default reset --------------");
-    geom.compute_with_atomic_counter_out_channel(|bfs| bfs.reset(), &mut ProgressLogger::default());
 
     info!("-------------- Reset no rayon --------------");
     geom.compute_with_atomic_counter_out_channel(|bfs| bfs.reset_no_rayon(), &mut ProgressLogger::default());
 
     info!("-------------- Reset min_len_iter 10000 --------------");
     geom.compute_with_atomic_counter_out_channel(|bfs| bfs.reset_min_len_iter(10_000), &mut ProgressLogger::default());
-
-    info!("-------------- Reset min_len_iter 10000000 --------------");
-    geom.compute_with_atomic_counter_out_channel(|bfs| bfs.reset_min_len_iter(10_000_000), &mut ProgressLogger::default());
 
     info!("\nDone");
 }

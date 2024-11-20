228de5c53deba9a41f16c647a1a5901459cc3645
Matteo Zagheno <matteo2309@hotmail.com>
2024-11-20 15:52:42 +0100

    added cli args

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,48 @@
 use crate::geometric_centralities::GeometricCentralities;
+use clap::Parser;
 use dsi_progress_logger::ProgressLogger;
 use log::info;
-use std::env;
 use webgraph::prelude::BvGraph;
-use webgraph_algo::traits::Sequential;
+
 
 mod geometric_centralities;
 
-fn main() {
-    env_logger::init();
-    let args: Vec<_> = env::args().collect();
-    let graph = BvGraph::with_basename(args.get(1).expect("Missing path argument"))
+
+#[derive(Parser, Debug)]
+#[command(about = "Benchmarks geometric centralities", long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    path: String,
+
+    #[clap(long, short)]
+    generic: bool,
+
+    #[clap(long, short)]
+    no_generic: bool,
+}
+
+fn main() -> anyhow::Result<()> {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .try_init()?;
+
+    let args = Args::parse();
+    let graph = BvGraph::with_basename(args.path)
         .load()
         .expect("Failed loading graph");
     let mut geom = GeometricCentralities::new(&graph, 0);
 
-    info!("-------------- Geom generic --------------");
-    geom.compute_with_atomic_counter_out_channel_generic(&mut ProgressLogger::default());
+    if args.generic {
+        info!("-------------- Geom generic --------------");
+        geom.compute_with_atomic_counter_out_channel_generic(&mut ProgressLogger::default());
+    }
 
-    info!("-------------- Geom no generic --------------");
-    geom.compute_with_atomic_counter_out_channel(&mut ProgressLogger::default());
+    if args.no_generic {
+        info!("-------------- Geom no generic --------------");
+        geom.compute_with_atomic_counter_out_channel(&mut ProgressLogger::default());
+    }
 
-    info!("\nDone");
+    info!("Done");
+
+    Ok(())
 }
